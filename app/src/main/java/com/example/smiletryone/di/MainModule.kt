72bb0b5f3903a6300cf79abb71a60e96046a3d7c
Faisal-style <faisalrezarahmat1@gmail.com>
2023-06-08 21:36:47 +0700

    add Network Request

--- app/src/main/java/com/example/smiletryone/di/MainModule.kt
+++ app/src/main/java/com/example/smiletryone/di/MainModule.kt
@@ -2,11 +2,18 @@ package com.example.smiletryone.di
 
 import android.content.Context
 import com.example.smiletryone.data.DataStoreRepository
+import com.example.smiletryone.data.remote.SmileApi
+import com.example.smiletryone.repository.SmileRepository
+import com.example.smiletryone.util.Constant.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,4 +24,26 @@ object MainModule {
     fun provideDataStoreRepository(
         @ApplicationContext context: Context
     ) = DataStoreRepository(context = context)
+
+    @Provides
+    @Singleton
+    fun getSmileApi(): SmileApi {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(SmileApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSmileRepository(api: SmileApi): SmileRepository {
+        return SmileRepository(api)
+    }
 }
