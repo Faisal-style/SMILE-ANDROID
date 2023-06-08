1ec1fc7f4e99ad6513c400b1b9aabc4dab00df92
Faisal-style <faisalrezarahmat1@gmail.com>
2023-06-08 21:29:46 +0700

    add LoginResponse

---
+++ app/src/main/java/com/example/smiletryone/data/remote/responses/LoginResponse.kt
@@ -0,0 +1,30 @@
+package com.example.smiletryone.data.remote.responses
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+
+	@field:SerializedName("loginResult")
+	val loginResult: LoginResult,
+
+	@field:SerializedName("message")
+	val message: String,
+
+	@field:SerializedName("status")
+	val status: String
+)
+
+data class LoginResult(
+
+	@field:SerializedName("role")
+	val role: String,
+
+	@field:SerializedName("id")
+	val id: Int,
+
+	@field:SerializedName("accessToken")
+	val accessToken: String,
+
+	@field:SerializedName("email")
+	val email: String
+)
